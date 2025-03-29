b5cfc5193742e2be1d7ead920c9cef6d654469f1
Mohd Faiz <mohdfaiz.1842@gmail.com>
2025-03-29 09:38:59 +0530

    add www

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "https://front-architectus.vercel.app", 'https://architectusbureau.com'],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://front-architectus.vercel.app", 'https://architectusbureau.com','https://www.architectusbureau.com'],
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
 }));
